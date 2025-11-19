e8074b1e285ec3b0e96ad60c3ac17a06004e74a3
nzia1-tech <nzia1@terpmail.umd.edu>
2025-11-19 17:31:05 -0500

    Color Sensor, Servo, and Ultrasonic code inclusion
    11-19-25 included basic code for Color sensor, servo, and ultrasonic. This still needs to be edited and shifted around for the mission.

--- Water/Water.ino
+++ Water/Water.ino
@@ -1,9 +1,39 @@
 #include <Enes100.h>
 
 #include <Servo.h>
+
+#include <NewPing.h>
+
 #include "Arduino.h"
 #include "Enes100.h"
 #include "Tank.h"
+
+//Servo:
+Servo ArmServo;
+
+int pos = 0; //will change based on what degree we want
+int inital_pos = 0;
+
+//Ultrasonic:
+const int TrigPin = 0; //note: only need one of these, as it sends out signal from ultrasonic
+const int EchoPin = 1;               // Arduino pin tied to echo pin on the ultrasonic sensor.
+const int MAX_DISTANCE = 50; // Maximum distance we want to ping for (in centimeters). Maximum sensor distance is rated at 400-500cm.
+
+#define SONAR_NUM     1 // Number of sensors.
+#define PING_INTERVAL 33 // Milliseconds between sensor pings (29ms is about the min to avoid cross-sensor echo).
+
+NewPing sonar(TrigPin, EchoPin, MAX_DISTANCE); // NewPing setup of pins .
+
+// color sensor:
+//#define s0 4        //Module pins  wiring
+//#define s1 5
+#define s2 5
+#define s3 6
+#define out 7
+
+bool Pollutants = false;
+
+int  Red=0, Blue=0, Green=0;  //RGB values 
 
 int sign(float number) {
     if (number >= 0) {
@@ -21,7 +51,22 @@ void setup() {
     Enes100.begin("CRyan Me A River", WATER, 12, 1116, 8, 9);
     // At this point we know we are connected.
     Enes100.println("Connected...");
-     Tank.begin();
+    Tank.begin();
+
+    //Servo:
+    ArmServo.attach(9);
+
+    //color sensor:
+    pinMode(s0,OUTPUT);    //pin modes
+    pinMode(s1,OUTPUT);
+    pinMode(s2,OUTPUT);
+    pinMode(s3,OUTPUT);
+    pinMode(out,INPUT);
+
+    Serial.begin(9600);   //intialize the serial monitor  baud rate
+   
+    digitalWrite(s0,HIGH); //Putting S0/S1 on HIGH/HIGH levels  means the output frequency scalling is at 100% (recommended)
+    digitalWrite(s1,HIGH);  //LOW/LOW is off HIGH/LOW is 20% and LOW/HIGH is  2%
 }
 
 void loop() {
@@ -34,6 +79,51 @@ void loop() {
     y = Enes100.getY();  // Your Y coordinate! 0-2, in meters, also -1 if your aruco is not visible.
     t = Enes100.getTheta();  //Your theta! -pi to +pi, in radians, -1 if your aruco is not visible.
     v = Enes100.isVisible(); // Is your aruco visible? True or False.
+
+
+
+
+    //Ultrasonic:
+    delay(50);       // Wait 50ms between pings ( 20 pings/sec). 29ms should be the shortest delay between pings.
+    unsigned int uS = sonar.ping(); // Send ping, get ping time in microseconds (uS).
+    //only need if we have serial.begin
+    //Serial.print("Ping: ");
+    //Serial.print(uS / US_ROUNDTRIP_CM); // Convert ping time to distance and print result (0 = outside set distance range, no ping echo)
+    //Serial.println("cm");
+
+
+    //color sensor:
+      
+    GetColors();                                     //Execute the GetColors function  to get the value of each RGB color
+                                                    //Depending  of the RGB values given by the sensor we can define the color and displays it on  the monitor
+
+    if (Red<Blue && Red<=Green && Red<78)  {    //if  Red value is the lowest one and smaller thant 23 it's likely Red
+        Serial.println("Red");
+    }
+
+    else {
+        Serial.println("Unknown");                  //if the color is not recognized, you can add as many as you want
+    }
+    if (Red<Blue && Red<=Green && Red<78){ //original: 23 ; 50 max? : 255   SET: 78
+        Serial.println("Pollutants Detected");
+    }
+    delay(2000);                                   //2s delay you can modify if you  want
+
+  //Note: for the values, man need to increace the detection values for things like red (ex: Red<23 up to Red<60) in order to have more range of detection (at costs of accuracy). 
+  //Note: the higher the frequency --> the lower the data we get printed. This means that if we get low data, then the lowest color detecting data value menas that that ci=olor is likely the one in front of the color sensor. 
+
+  //Servo:
+   //if ([need a function when reach watertank]){
+    for (pos = 0; pos = 180; pos += 1) { //position 0 is assumed to be straight up. pos 90 is 90 degrees (pi/2) and should be right above the tank (may need to change). move at a incrament of 1 degree
+      ArmServo.write(pos);
+      delay (15000);
+      inital_pos = 0;
+      ArmServo.write(inital_pos);
+      delay (10000);
+      //can adjust based on the water tank, and if collection of water is marked as complete. (happens after water depth and pollutant detection)
+    }
+  //}
+}
 
     if (v) // If the ArUco marker is visible
     {
@@ -208,6 +298,24 @@ void moveToY(float y) {
     Tank.turnOffMotors();
     return;
 }
+
+//color sensor how to get color:
+void GetColors()  
+{    
+  digitalWrite(s2,  LOW);                                           //S2/S3 levels define which set  of photodiodes we are using LOW/LOW is for RED LOW/HIGH is for Blue and HIGH/HIGH  is for green 
+  digitalWrite(s3, LOW);                                           
+  Red = pulseIn(out, digitalRead(out) == HIGH ? LOW : HIGH);       //here we wait  until "out" go LOW, we start measuring the duration and stops when "out" is  HIGH again, if you have trouble with this expression check the bottom of the code
+  delay(20);  
+  digitalWrite(s3, HIGH);                                         //Here  we select the other color (set of photodiodes) and measure the other colors value  using the same techinque
+  Blue = pulseIn(out, digitalRead(out) == HIGH ? LOW  : HIGH);
+  delay(20);  
+  digitalWrite(s2, HIGH);  
+  Green = pulseIn(out,  digitalRead(out) == HIGH ? LOW : HIGH);
+  delay(20);  
+}
+
+//Note: the higher the frequency --> the lower the data we get printed
+
 
 //void loop() {
     //float Yval = Enes100.getY();
